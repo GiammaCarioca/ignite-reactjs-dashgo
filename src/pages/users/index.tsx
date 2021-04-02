b504481ba698e331863f5135d08fbd27d8041534
Thiago Giammattey <giammattey@gmail.com>
2021-04-02 14:47:42 -0300

    add page: users list

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,154 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text } from "@chakra-ui/react";
+
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+
+export default function Userlist() {
+  return (
+    <Box>
+      <Header />
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading fontSize="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" width="8" color="gray.300">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="6" />
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Giammattey</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      giammattey@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>02 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="whiteAlpha"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Giammattey</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      giammattey@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>02 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="whiteAlpha"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Giammattey</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      giammattey@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>02 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="whiteAlpha"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Giammattey</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      giammattey@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>02 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="whiteAlpha"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
