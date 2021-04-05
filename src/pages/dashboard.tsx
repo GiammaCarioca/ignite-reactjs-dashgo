8af9ad123658c79b19f1297de2a3337bc55844fb
Thiago Giammattey <giammattey@gmail.com>
2021-04-05 12:12:11 -0300

    responsive Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth={320} align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
               <Chart
@@ -80,7 +80,7 @@ export default function Dashboard() {
               />
             </Text>
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
               <Chart
