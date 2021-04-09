2ce500c9a7ece1850d0a92cf757e8422246fc4bc
Thiago Giammattey <giammattey@gmail.com>
2021-04-09 11:15:45 -0300

    configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,25 +1,24 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text, Spinner, useBreakpointValue } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { useQuery } from 'react-query'
 
 import Header from "../../components/Header";
-import Sidebar from "../../components/Sidebar";
-import { useEffect } from "react";
-
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import Pagination from "../../components/Pagination";
-import Link from "next/link";
-
+import Sidebar from "../../components/Sidebar";
 
 export default function Userlist() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -50,133 +49,146 @@ export default function Userlist() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" width="8" color="gray.300">
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de Cadastro</Th> }
-                <Th width="6" />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Thiago Giammattey</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      giammattey@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril de 2021</Td>}
-                
-                { isWideVersion &&
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                  </Button>
-                  </Td> }
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+                <Text>
+                  Falha ao obter dados dos usuários.
+                </Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" width="8" color="gray.300">
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de Cadastro</Th> }
+                    <Th width="6" />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Thiago Giammattey</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          giammattey@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>02 de Abril de 2021</Td>}
+                    
+                    { isWideVersion &&
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                      </Button>
+                      </Td> }
 
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Thiago Giammattey</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      giammattey@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril de 2021</Td>}
-                
-                {isWideVersion &&
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion ? 'Editar' : ''}
-                    </Button>
-                  </Td>
-                }
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Thiago Giammattey</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      giammattey@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril de 2021</Td>}
-                
-                {isWideVersion &&
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion ? 'Editar' : ''}
-                    </Button>
-                  </Td>
-                }
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Thiago Giammattey</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      giammattey@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>02 de Abril de 2021</Td>}
-                
-                {isWideVersion &&
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion ? 'Editar' : ''}
-                    </Button>
-                  </Td>
-                }
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Thiago Giammattey</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          giammattey@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>02 de Abril de 2021</Td>}
+                    
+                    {isWideVersion &&
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          {isWideVersion ? 'Editar' : ''}
+                        </Button>
+                      </Td>
+                    }
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Thiago Giammattey</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          giammattey@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>02 de Abril de 2021</Td>}
+                    
+                    {isWideVersion &&
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          {isWideVersion ? 'Editar' : ''}
+                        </Button>
+                      </Td>
+                    }
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Thiago Giammattey</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          giammattey@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>02 de Abril de 2021</Td>}
+                    
+                    {isWideVersion &&
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          {isWideVersion ? 'Editar' : ''}
+                        </Button>
+                      </Td>
+                    }
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
